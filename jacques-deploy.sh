6d7e77e738f2972b4b7e6e621a36065baebcefd1
Jacques.Millard <jacques@millard.za.org>
2025-10-28 12:22:25 +0200

    update nginx version to 1.29.2 in Dockerfile and deployment script

--- jacques-deploy.sh
+++ jacques-deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-VERSION=1.27.4-brotli
+VERSION=1.29.2-arm-brotli
 
 aws ecr get-login-password --region eu-west-1 --profile dstv-aws-master | docker login --username AWS --password-stdin 240247221724.dkr.ecr.eu-west-1.amazonaws.com
 
-docker build --platform linux/amd64 -t nginx_noheader .
+docker build -t nginx_noheader -f Dockerfile.arm .
 
 docker tag nginx_noheader:latest 240247221724.dkr.ecr.eu-west-1.amazonaws.com/nginx_noheader:latest
 docker tag nginx_noheader:latest 240247221724.dkr.ecr.eu-west-1.amazonaws.com/nginx_noheader:${VERSION}
